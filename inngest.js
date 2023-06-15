eb3715368689575fff4e706b46eaa21f815c5be3
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-15 11:53:41 +1000

    inngest test

--- inngest.js
+++ inngest.js
@@ -1,20 +1,20 @@
-const { Inngest } = require("inngest");
-const axios = require("axios");
+// const { Inngest } = require("inngest");
+// const axios = require("axios");
 
-const inngest = new Inngest({ name: "Test" });
+// const inngest = new Inngest({ name: "Test" });
 
-const fn = inngest.createFunction(
-  { name: "Trigger Vercel Function" },
-  { event: "get/brandfetch" }, // This should match the event name that you're sending to Inngest
-  async ({ event, step }) => {
-    await step.run("Trigger Vercel Function", async () => {
-      const res = await axios.post(
-        "https://www.instantcollab.co/api/brandFetch",
-        event.data
-      );
-      // Handle the response...
-    });
-  }
-);
+// const fn = inngest.createFunction(
+//   { name: "Trigger Vercel Function" },
+//   { event: "get/brandfetch" }, // This should match the event name that you're sending to Inngest
+//   async ({ event, step }) => {
+//     await step.run("Trigger Vercel Function", async () => {
+//       const res = await axios.post(
+//         "https://www.instantcollab.co/api/brandFetch",
+//         event.data
+//       );
+//       // Handle the response...
+//     });
+//   }
+// );
 
-module.exports = fn;
+// module.exports = fn;
