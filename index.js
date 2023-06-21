24c4f5704945396a9eb03d89bc43d0361aadb0fb
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-21 11:44:16 +1000

    testing imports

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// require("dotenv").config();
-// const express = require("express");
-// const apiClient = require("./apiClient");
-// const cors = require("cors");
+require("dotenv").config();
+const express = require("express");
+const apiClient = require("./apiClient");
+const cors = require("cors");
 
-// const app = express();
-// app.use(express.json()); // Middleware to parse JSON bodies
-// app.use(cors());
+const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors());
 
-// // app.use(
-// //   cors({
-// //     origin: "https://www.instantcollab.co",
-// //   })
-// // );
+// app.use(
+//   cors({
+//     origin: "https://www.instantcollab.co",
+//   })
+// );
 
-// app.post("/", async function (req, res) {
-//   const { userId } = req.body;
-//   console.log(userId); // Log the userId sent from your getMeetings function
-//   const meetings = await apiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
-//   res.send("Received your request!");
-// });
+app.post("/", async function (req, res) {
+  const { userId } = req.body;
+  console.log(userId); // Log the userId sent from your getMeetings function
+  const meetings = await apiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
+  res.send("Received your request!");
+});
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, "0.0.0.0", function () {
-//   console.log(`App is listening on IP 0.0.0.0 and port ${port}!`);
-// });
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
+  console.log(`App is listening on IP 0.0.0.0 and port ${port}!`);
+});
