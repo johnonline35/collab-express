a8b8a037050c1c0ae27899c0838a5a14a9c2c257
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-27 09:46:48 +1000

    dashboard and collab page ui updates

--- src/routes/calendarRoutes.js
+++ src/routes/calendarRoutes.js
@@ -5,8 +5,13 @@ const googleCalendarApiClient = require("../api/googleCalendarApiClient");
 router.post("/", async (req, res) => {
   const { userId } = req.body;
   console.log(userId); // Log the userId sent from your getMeetings function
-  const meetings = await googleCalendarApiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
-  res.send("Received your request!");
+  try {
+    const meetings = await googleCalendarApiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
+    res.json(meetings); // Send meetings data as the response
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching meetings");
+  }
 });
 
 module.exports = router;
