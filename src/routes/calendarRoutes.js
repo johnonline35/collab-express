87d1b84fefacc4914200c62998f7e6b420cfb2a9
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-06-27 11:29:52 +1000

    dashboard and collab page ui updates

--- src/routes/calendarRoutes.js
+++ src/routes/calendarRoutes.js
@@ -7,7 +7,14 @@ router.post("/", async (req, res) => {
   console.log(userId); // Log the userId sent from your getMeetings function
   try {
     const meetings = await googleCalendarApiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
-    res.json(meetings); // Send meetings data as the response
+
+    // Create the response object
+    const response = {
+      workspace_id: meetings.length > 0 ? meetings[0].workspace_id : null,
+      meetings: meetings,
+    };
+
+    res.json(response); // Send the response including workspace_id and meetings data
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching meetings");
@@ -15,3 +22,21 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const googleCalendarApiClient = require("../api/googleCalendarApiClient");
+
+// router.post("/", async (req, res) => {
+//   const { userId } = req.body;
+//   console.log(userId); // Log the userId sent from your getMeetings function
+//   try {
+//     const meetings = await googleCalendarApiClient.getGoogleCal(userId); // Pass userId to your getGoogleCal function
+//     res.json(meetings); // Send meetings data as the response
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error fetching meetings");
+//   }
+// });
+
+// module.exports = router;
--- src/services/meetingAnalysis.js
+++ src/services/meetingAnalysis.js
@@ -52,8 +52,16 @@ const analyzeMeetings = async (userId) => {
         let workspaceId;
         if (publicEmailDomains.includes(domain)) {
           workspaceId = await handlePublicDomain(email, userId);
+          if (workspaceId) {
+            // Append the workspaceId to the meeting object
+            meeting.workspace_id = workspaceId;
+          }
         } else {
           workspaceId = await handlePrivateDomain(email, userId);
+          if (workspaceId) {
+            // Append the workspaceId to the meeting object
+            meeting.workspace_id = workspaceId;
+          }
         }
 
         if (!workspaceId) continue;
@@ -78,69 +86,25 @@ module.exports = {
   analyzeMeetings,
 };
 
-// const fetchPublicEmailDomains = require("../data/listOfEmailDomains");
-// const { checkAttendee } = require("../models/attendees");
-// const {
-//   handlePublicDomain,
-//   handlePrivateDomain,
-// } = require("../handlers/workspaceHandler");
-// const {
-//   fetchFutureMeetings,
-//   fetchPastMeetings,
-//   fetchMeetingDetails,
-//   fetchMeetingAttendees,
-// } = require("../models/meetingsModel");
-// const { filterCollabUserEmails } = require("./emailFilterService");
-
-// const analyzeMeetings = async (userId) => {
-//   try {
-//     let nextMeetings = [];
-
-//     // 1. Find the next meetings from time = now, from the calendar start_dateTime
-//     const futureMeetings = await fetchFutureMeetings();
-//     nextMeetings = nextMeetings.concat(futureMeetings);
-
-//     // If fewer than 8 meetings are fetched, find meetings from the past
-//     if (nextMeetings.length < 8) {
-//       const pastMeetings = await fetchPastMeetings(nextMeetings.length);
-//       nextMeetings = nextMeetings.concat(pastMeetings);
-//     }
-
-//     // For each meeting, create a list of creator_email, organizer_email, and attendee emails
-//     for (let meeting of nextMeetings) {
-//       const meetingDetails = await fetchMeetingDetails(meeting.id);
-//       if (!meetingDetails) continue;
-
-//       const attendees = await fetchMeetingAttendees(meeting.id);
-//       if (!attendees) continue;
-
-//       // Create an array of all emails for the current meeting
-//       let emails = [
-//         meetingDetails[0].creator_email,
-//         meetingDetails[0].organizer_email,
-//         ...attendees.map((attendee) => attendee.email),
-//       ];
-
-//       // Filter out emails that match with collab_user
-//       emails = await filterCollabUserEmails(emails);
+//  emails = await filterCollabUserEmails(emails);
 
 //       for (let email of emails) {
 //         let domain = email.split("@")[1];
 
-//         if (fetchPublicEmailDomains.includes(domain)) {
-//           console.log("domain:", domain);
-//           const workspaceId = await handlePublicDomain(email, userId);
-//           if (!workspaceId) continue;
-
-//           const attendeeId = await checkAttendee(email, workspaceId);
-//           if (!attendeeId) continue;
+//         let workspaceId;
+//         if (publicEmailDomains.includes(domain)) {
+//           workspaceId = await handlePublicDomain(email, userId);
 //         } else {
-//           const workspaceId = await handlePrivateDomain(email, userId);
-//           if (!workspaceId) continue;
-
-//           const attendeeId = await checkAttendee(email, workspaceId);
-//           if (!attendeeId) continue;
+//           workspaceId = await handlePrivateDomain(email, userId);
 //         }
+
+//         if (!workspaceId) continue;
+
+//         const attendeeId = await checkAttendee(email, userId, workspaceId);
+//         if (!attendeeId) continue;
+
+//         // Call the assignWorkspaceLead function here
+//         await assignWorkspaceLead(email, workspaceId);
 //       }
 //     }
 
