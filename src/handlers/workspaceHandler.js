5dfb44abb54c55bba50213b67120b6bf8551e05e
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-06-27 11:58:26 +1000

    dashboard and collab page ui updates

--- src/handlers/workspaceHandler.js
+++ src/handlers/workspaceHandler.js
@@ -15,6 +15,9 @@ const handlePublicDomain = async (email, userId) => {
     .select("workspace_id")
     .eq("meeting_attendee_email", email);
 
+  console.log("handlePublicDomain data:", data);
+  console.log("handlePublicDomain error:", error);
+
   if (error) {
     console.error("Error fetching workspace:", error);
     return null;
@@ -44,6 +47,7 @@ const handlePublicDomain = async (email, userId) => {
     workspaceId = data[0].workspace_id;
   }
 
+  console.log("handlePublicDomain workspaceId:", workspaceId);
   return workspaceId;
 };
 
@@ -59,6 +63,9 @@ const handlePrivateDomain = async (email, userId) => {
     .from("workspaces")
     .select("workspace_id")
     .eq("domain", domain);
+
+  console.log("handlePrivateDomain data:", data);
+  console.log("handlePrivateDomain error:", error);
 
   if (error) {
     console.error("Error fetching workspace:", error);
@@ -89,6 +96,7 @@ const handlePrivateDomain = async (email, userId) => {
     workspaceId = data[0].workspace_id;
   }
 
+  console.log("handlePrivateDomain workspaceId:", workspaceId);
   return workspaceId;
 };
 
@@ -96,3 +104,99 @@ module.exports = {
   handlePublicDomain,
   handlePrivateDomain,
 };
+
+// const handlePublicDomain = async (email, userId) => {
+//   const domain = email.split("@")[1];
+//   console.log("handlePublicDomain email:", email);
+//   console.log("handlePublicDomain domain:", domain);
+//   let workspaceName = email
+//     .split("@")[0]
+//     .replace(".", " ")
+//     .replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
+
+//   let { data, error } = await supabase
+//     .from("workspaces")
+//     .select("workspace_id")
+//     .eq("meeting_attendee_email", email);
+
+//   if (error) {
+//     console.error("Error fetching workspace:", error);
+//     return null;
+//   }
+
+//   let workspaceId;
+//   if (data.length === 0) {
+//     // Workspace not found, create a new one
+//     workspaceId = uuidv4();
+
+//     let workspaceData = {
+//       workspace_id: workspaceId,
+//       meeting_attendee_email: email,
+//       workspace_name: workspaceName,
+//       collab_user_id: userId, // Use userId here
+//     };
+
+//     let { data: upsertData, error: workspaceUpsertError } = await supabase
+//       .from("workspaces")
+//       .upsert([workspaceData]);
+
+//     if (workspaceUpsertError) {
+//       console.error("Error upserting workspace:", workspaceUpsertError);
+//       return null;
+//     }
+//   } else {
+//     workspaceId = data[0].workspace_id;
+//   }
+
+//   return workspaceId;
+// };
+
+// const handlePrivateDomain = async (email, userId) => {
+//   const domain = email.split("@")[1];
+//   console.log("handlePrivateDomain email:", email);
+//   console.log("handlePrivateDomain domain:", domain);
+//   let workspaceName = domain.split(".")[0];
+//   workspaceName =
+//     workspaceName.charAt(0).toUpperCase() + workspaceName.slice(1); // Capitalize the first letter
+
+//   let { data, error } = await supabase
+//     .from("workspaces")
+//     .select("workspace_id")
+//     .eq("domain", domain);
+
+//   if (error) {
+//     console.error("Error fetching workspace:", error);
+//     return null;
+//   }
+
+//   let workspaceId;
+//   if (data.length === 0) {
+//     // Workspace not found, create a new one
+//     workspaceId = uuidv4();
+
+//     let workspaceData = {
+//       workspace_id: workspaceId,
+//       workspace_name: workspaceName,
+//       domain: domain,
+//       collab_user_id: userId,
+//     };
+
+//     let { data: upsertData, error: workspaceUpsertError } = await supabase
+//       .from("workspaces")
+//       .upsert([workspaceData]);
+
+//     if (workspaceUpsertError) {
+//       console.error("Error upserting workspace:", workspaceUpsertError);
+//       return null;
+//     }
+//   } else {
+//     workspaceId = data[0].workspace_id;
+//   }
+
+//   return workspaceId;
+// };
+
+// module.exports = {
+//   handlePublicDomain,
+//   handlePrivateDomain,
+// };
