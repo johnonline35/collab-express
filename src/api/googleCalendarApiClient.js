9dccdbd61ea6023b8f731ccabf41cdb2230b75b9
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-07-24 10:49:14 +1000

    complete meeting fetch and store rewrite using sync tokens

--- src/api/googleCalendarApiClient.js
+++ src/api/googleCalendarApiClient.js
@@ -22,6 +22,15 @@ const getGoogleCal = async (userId) => {
     console.log("userEmail:", userEmail);
   }
 
+  const now = new Date();
+  const xMonthsAgo = new Date(now.getTime());
+  xMonthsAgo.setMonth(now.getMonth() - 6);
+  const timeMin = xMonthsAgo.toISOString();
+
+  const xMonthsFromNow = new Date(now.getTime());
+  xMonthsFromNow.setMonth(now.getMonth() + 4);
+  const timeMax = xMonthsFromNow.toISOString();
+
   let allEvents = [];
   let nextPageToken = undefined;
   let nextSyncToken = undefined;
