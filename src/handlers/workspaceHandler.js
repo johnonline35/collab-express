3f1b6eba7a4ebeeca191c123b55b0cfafab63d8f
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-07-28 10:39:14 +1000

    single events set to false

--- src/handlers/workspaceHandler.js
+++ src/handlers/workspaceHandler.js
@@ -6,55 +6,51 @@ function assignWorkspaceLead(attendeesForThisMeeting, meeting) {
 
   console.log(`Meeting ID: ${meeting.id}`);
 
-  let organizers = [];
-  let creators = [];
-  let acceptedAttendees = [];
-  let needsActionAttendees = [];
-  let declinedAttendees = [];
+  let organizer = null;
+  let creator = null;
+  let accepted = null;
   let firstAttendee = attendeesForThisMeeting[0];
 
-  // Iterate over attendees and use cascading logic
+  // Iterate over all attendees
   for (let attendee of attendeesForThisMeeting) {
+    // Logging attendee
     console.log(`Evaluating attendee: ${JSON.stringify(attendee)}`);
 
+    // Determine and log attendee's role
     if (attendee.email === meeting.organizer_email) {
-      organizers.push(attendee);
-      console.log(`Organizer found: ${JSON.stringify(attendee)}`);
+      console.log(`Attendee is an organizer: ${JSON.stringify(attendee)}`);
+      organizer = organizer || attendee;
     }
 
     if (attendee.email === meeting.creator_email) {
-      creators.push(attendee);
-      console.log(`Creator found: ${JSON.stringify(attendee)}`);
+      console.log(`Attendee is a creator: ${JSON.stringify(attendee)}`);
+      creator = creator || attendee;
     }
 
     if (attendee.response_status === "accepted") {
-      acceptedAttendees.push(attendee);
-      console.log(`Accepted attendee found: ${JSON.stringify(attendee)}`);
+      console.log(`Attendee has accepted: ${JSON.stringify(attendee)}`);
+      accepted = accepted || attendee;
     }
 
     if (attendee.response_status === "needsAction") {
-      needsActionAttendees.push(attendee);
       console.log(`Attendee needs action: ${JSON.stringify(attendee)}`);
     }
 
     if (attendee.response_status === "declined") {
-      declinedAttendees.push(attendee);
-      console.log(`Attendee declined: ${JSON.stringify(attendee)}`);
+      console.log(`Attendee has declined: ${JSON.stringify(attendee)}`);
     }
   }
 
   // Assign lead based on priority
-  if (organizers.length > 0) {
-    console.log(`Assigned lead (organizer): ${JSON.stringify(organizers[0])}`);
-    return organizers[0];
-  } else if (creators.length > 0) {
-    console.log(`Assigned lead (creator): ${JSON.stringify(creators[0])}`);
-    return creators[0];
-  } else if (acceptedAttendees.length > 0) {
-    console.log(
-      `Assigned lead (accepted): ${JSON.stringify(acceptedAttendees[0])}`
-    );
-    return acceptedAttendees[0];
+  if (organizer) {
+    console.log(`Assigned lead (organizer): ${JSON.stringify(organizer)}`);
+    return organizer;
+  } else if (creator) {
+    console.log(`Assigned lead (creator): ${JSON.stringify(creator)}`);
+    return creator;
+  } else if (accepted) {
+    console.log(`Assigned lead (accepted): ${JSON.stringify(accepted)}`);
+    return accepted;
   } else if (firstAttendee) {
     console.log(
       `Assigned lead (first attendee): ${JSON.stringify(firstAttendee)}`
@@ -64,6 +60,72 @@ function assignWorkspaceLead(attendeesForThisMeeting, meeting) {
 
   throw new Error("No suitable lead found");
 }
+
+// function assignWorkspaceLead(attendeesForThisMeeting, meeting) {
+//   if (!attendeesForThisMeeting || attendeesForThisMeeting.length === 0) {
+//     throw new Error("No attendees available to assign as lead.");
+//   }
+
+//   console.log(`Meeting ID: ${meeting.id}`);
+
+//   let organizers = [];
+//   let creators = [];
+//   let acceptedAttendees = [];
+//   let needsActionAttendees = [];
+//   let declinedAttendees = [];
+//   let firstAttendee = attendeesForThisMeeting[0];
+
+//   // Iterate over attendees and use cascading logic
+//   for (let attendee of attendeesForThisMeeting) {
+//     console.log(`Evaluating attendee: ${JSON.stringify(attendee)}`);
+
+//     if (attendee.email === meeting.organizer_email) {
+//       organizers.push(attendee);
+//       console.log(`Organizer found: ${JSON.stringify(attendee)}`);
+//     }
+
+//     if (attendee.email === meeting.creator_email) {
+//       creators.push(attendee);
+//       console.log(`Creator found: ${JSON.stringify(attendee)}`);
+//     }
+
+//     if (attendee.response_status === "accepted") {
+//       acceptedAttendees.push(attendee);
+//       console.log(`Accepted attendee found: ${JSON.stringify(attendee)}`);
+//     }
+
+//     if (attendee.response_status === "needsAction") {
+//       needsActionAttendees.push(attendee);
+//       console.log(`Attendee needs action: ${JSON.stringify(attendee)}`);
+//     }
+
+//     if (attendee.response_status === "declined") {
+//       declinedAttendees.push(attendee);
+//       console.log(`Attendee declined: ${JSON.stringify(attendee)}`);
+//     }
+//   }
+
+//   // Assign lead based on priority
+//   if (organizers.length > 0) {
+//     console.log(`Assigned lead (organizer): ${JSON.stringify(organizers[0])}`);
+//     return organizers[0];
+//   } else if (creators.length > 0) {
+//     console.log(`Assigned lead (creator): ${JSON.stringify(creators[0])}`);
+//     return creators[0];
+//   } else if (acceptedAttendees.length > 0) {
+//     console.log(
+//       `Assigned lead (accepted): ${JSON.stringify(acceptedAttendees[0])}`
+//     );
+//     return acceptedAttendees[0];
+//   } else if (firstAttendee) {
+//     console.log(
+//       `Assigned lead (first attendee): ${JSON.stringify(firstAttendee)}`
+//     );
+//     return firstAttendee;
+//   }
+
+//   throw new Error("No suitable lead found");
+// }
 
 function createWorkspaceName(leadEmail, publicEmailDomains) {
   // Ensure leadEmail is valid
