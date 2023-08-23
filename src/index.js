4149eadbe299565e969f83a721cbcf983208794d
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-23 13:42:49 +1000

    added openai

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Initialize dotenv
 dotenv.config();
+
 // Initialize Express
 const app = express();
-
-// Initialize dotenv
 
 // Internal Modules
 const corsMiddleware = require("./middleware/cors");
 const calendarRoutes = require("./routes/calendarRoutes");
-// const { analyzeMeetings } = require("./services/meetingAnalysis");
+const openAiRoutes = require("./routes/openAiRoutes");
 
 // Middlewares
 app.use(express.json());
@@ -19,11 +19,7 @@ app.use(corsMiddleware);
 
 // Routes
 app.use("/", calendarRoutes);
-
-console.log("Index.js script started.");
-
-// Error Handling Middleware
-// ...
+app.use("/", openAiRoutes);
 
 // Start Server
 const port = process.env.PORT || 3000;
@@ -31,9 +27,3 @@ const host = "0.0.0.0";
 app.listen(port, host, () => {
   console.log(`App is listening on IP ${host} and port ${port}!`);
 });
-
-// analyzeMeetings("fb7b190c-27b4-4969-b591-5d2e9cd79dc9")
-//   .then((meetings) => {
-//     console.log(meetings);
-//   })
-//   .catch(console.error);
