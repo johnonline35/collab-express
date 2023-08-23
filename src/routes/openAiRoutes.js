69c590241b14cbd07b8e89652eeeed0d14747687
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-23 13:56:42 +1000

    added openai

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -7,6 +7,15 @@ router.post("/summarize-career-education", async (req, res) => {
   console.log("Request received for /summarize-career-education"); // To confirm the endpoint is being hit
 
   try {
+    // Fetch data from Supabase
+    // const careerData = await supabase.from("career_table").select("*");
+    // const educationData = await supabase.from("education_table").select("*");
+
+    // const prompt = convertToReadableText(careerData, educationData);
+    //
+    const completionPrompt = `Based on the education and career information provided: went to stanford, studied medicine, is a billionare, please list three rapport-building topics that could be used in conversation. Each topic should be a sentence or two and relate specifically to the individual's experiences or background.
+`;
+
     // Use OpenAI to summarize the text
     console.log("About to call OpenAI with the prompt: ", completionPrompt);
     const completion = await openAi.createCompletion(
