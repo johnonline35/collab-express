43f0f4ab28796eb6e6de54b4742fc8aff21f77ba
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-23 13:55:56 +1000

    added openai

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -4,17 +4,11 @@ const supabase = require("../db/supabase");
 const openAi = require("../api/openAi");
 
 router.post("/summarize-career-education", async (req, res) => {
+  console.log("Request received for /summarize-career-education"); // To confirm the endpoint is being hit
+
   try {
-    // Fetch data from Supabase
-    // const careerData = await supabase.from("career_table").select("*");
-    // const educationData = await supabase.from("education_table").select("*");
-
-    // const prompt = convertToReadableText(careerData, educationData);
-    //
-    const completionPrompt = `Based on the education and career information provided: went to stanford, studied medicine, is a billionare, please list three rapport-building topics that could be used in conversation. Each topic should be a sentence or two and relate specifically to the individual's experiences or background.
-`;
-
     // Use OpenAI to summarize the text
+    console.log("About to call OpenAI with the prompt: ", completionPrompt);
     const completion = await openAi.createCompletion(
       {
         model: "text-davinci-004",
@@ -22,12 +16,20 @@ router.post("/summarize-career-education", async (req, res) => {
         max_tokens: 350,
         stream: true,
       },
-      { responseType: "stream" }
+      {
+        responseType: "stream",
+      }
     );
+
+    console.log("Received completion from OpenAI:", completion);
 
     res.json(completion);
   } catch (error) {
-    console.error(error);
+    console.error("Caught an error:", error); // This will give a detailed error message.
+    if (error.response) {
+      // If error response from OpenAI, print out the full error response
+      console.error("OpenAI API error:", error.response.data);
+    }
     res.status(500).send("Error summarizing data");
   }
 });
