282b2ba6b7dbf05cb41ba2c24edb47fb62480eeb
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-23 15:00:47 +1000

    added openai

--- src/api/openAi.js
+++ src/api/openAi.js
@@ -1,6 +1,5 @@
 const OpenAI = require("openai");
 const { OpenAiApiKey } = require("../config");
-// const openAi = new openai.OpenAIApi(OpenAIApiKey);
 
 const openai = new OpenAI({
   apiKey: OpenAiApiKey,
