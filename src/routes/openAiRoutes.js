fc99bf36bcb41c475c794903dafe0db51bb06cf9
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-23 16:38:42 +1000

    created insert build rapport command

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -4,14 +4,6 @@ const supabase = require("../db/supabase");
 const openai = require("../api/openAi");
 
 router.post("/summarize-career-education", async (req, res) => {
-  //   try {
-  //     // Fetch data from Supabase
-  //     // const careerData = await supabase.from("career_table").select("*");
-  //     // const educationData = await supabase.from("education_table").select("*");
-
-  //     // const prompt = convertToReadableText(careerData, educationData);
-  //     //
-
   async function testChat() {
     const completion = await openai.chat.completions.create({
       model: "gpt-4",
@@ -23,43 +15,54 @@ router.post("/summarize-career-education", async (req, res) => {
         },
         { role: "user", content: "Hello!" },
       ],
+      max_tokens: 350,
       stream: true,
     });
 
+    let responseContent = "";
     for await (const chunk of completion) {
+      responseContent += chunk.choices[0].delta.content;
       console.log(chunk.choices[0].delta.content);
     }
+    res.json({ content: responseContent });
   }
-
   testChat();
-  // const completionPrompt = `Based on the education and career information provided: went to stanford, studied medicine, is a billionare, please list three rapport-building topics that could be used in conversation. Each topic should be a sentence or two and relate specifically to the individual's experiences or background.
-  // `;
-
-  // //     // Use OpenAI to summarize the text
-  // console.log("About to call OpenAI with the prompt: ", completionPrompt);
-  // const completion = await openai.chat.completions.create(
-  //   {
-  //     model: "text-davinci-004",
-  //     prompt: completionPrompt,
-  //     max_tokens: 350,
-  //     stream: true,
-  //   },
-  //   {
-  //     responseType: "stream",
-  //   }
-  // );
-
-  // console.log("Received completion from OpenAI:", completion);
-
-  //     res.json(completion);
-  //   } catch (error) {
-  //     console.error("Caught an error:", error); // This will give a detailed error message.
-  //     if (error.response) {
-  //       // If error response from OpenAI, print out the full error response
-  //       console.error("OpenAI API error:", error.response.data);
-  //     }
-  //     res.status(500).send("Error summarizing data");
-  //   }
 });
 
 module.exports = router;
+
+//   try {
+//     // Fetch data from Supabase
+//     // const careerData = await supabase.from("career_table").select("*");
+//     // const educationData = await supabase.from("education_table").select("*");
+
+//     // const prompt = convertToReadableText(careerData, educationData);
+//     //
+// const completionPrompt = `Based on the education and career information provided: went to stanford, studied medicine, is a billionare, please list three rapport-building topics that could be used in conversation. Each topic should be a sentence or two and relate specifically to the individual's experiences or background.
+// `;
+
+// //     // Use OpenAI to summarize the text
+// console.log("About to call OpenAI with the prompt: ", completionPrompt);
+// const completion = await openai.chat.completions.create(
+//   {
+//     model: "text-davinci-004",
+//     prompt: completionPrompt,
+//     max_tokens: 350,
+//     stream: true,
+//   },
+//   {
+//     responseType: "stream",
+//   }
+// );
+
+// console.log("Received completion from OpenAI:", completion);
+
+//     res.json(completion);
+//   } catch (error) {
+//     console.error("Caught an error:", error); // This will give a detailed error message.
+//     if (error.response) {
+//       // If error response from OpenAI, print out the full error response
+//       console.error("OpenAI API error:", error.response.data);
+//     }
+//     res.status(500).send("Error summarizing data");
+//   }
