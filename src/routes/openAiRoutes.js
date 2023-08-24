c75c821542f9d1f0af50fee815bf9c4ca49ffa9b
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-25 08:59:53 +1000

    created insert build rapport command

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -1,78 +1,84 @@
 const express = require("express");
 const router = express.Router();
-const { fetchCompletionFromOpenAI } = require("../api/openAi");
-const { fetchAllAttendeeInfos } = require("../utils/database");
-const { createCompletionPrompt } = require("../utils/helper");
-
-router.post("/summarize-career-education", async (req, res) => {
-  const meetingData = req.body[0];
-  const attendees = meetingData.attendees;
-
-  const attendeeInfos = await fetchAllAttendeeInfos(attendees);
-
-  for (let i = 0; i < attendeeInfos.length; i++) {
-    const completionPrompt = createCompletionPrompt(
-      attendeeInfos[i],
-      attendees[i]
-    );
-    console.log("Completion prompt:", completionPrompt);
-    try {
-      const responseContent = await fetchCompletionFromOpenAI(completionPrompt);
-      res.json({ content: responseContent });
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
-  }
-});
+const { openai, fetchCompletionFromOpenAI } = require("../api/openAi");
+const {
+  fetchAttendeeData,
+  fetchAllAttendeeInfos,
+} = require("../utils/database");
+const {
+  convertToReadableText,
+  createCompletionPrompt,
+} = require("../utils/helper");
 
 // router.post("/summarize-career-education", async (req, res) => {
 //   const meetingData = req.body[0];
 //   const attendees = meetingData.attendees;
-//   const attendeeInfos = [];
 
-//   for (let attendee of attendees) {
-//     const attendeeInfo = await fetchAttendeeData(attendee.attendee_email);
-//     attendeeInfos.push(attendeeInfo);
-//   }
+//   const attendeeInfos = await fetchAllAttendeeInfos(attendees);
 
 //   for (let i = 0; i < attendeeInfos.length; i++) {
-//     const attendeeInfo = attendeeInfos[i];
-//     const attendee = attendees[i]; // Assuming the ordering has remained consistent
-
-//     const promptText = convertToReadableText(
-//       attendeeInfo.experience,
-//       attendeeInfo.education,
-//       attendee
+//     const completionPrompt = createCompletionPrompt(
+//       attendeeInfos[i],
+//       attendees[i]
 //     );
-
-//     const completionPrompt = `Based on the education and career information provided, please list three rapport-building topics that could be used in conversation. Please be descriptive and use the format as if you are talking to someone as in "I noticed..." or "Your ..." and then the specific observation you wish to share. Each topic should relate specifically to the individual's experiences or background - please focus on unusual things and broadly on why what the person has done in the past has impacted on what they do now. Do not use the same theme more than once. There might be a lot of repetition in experience, in which case, please summarize similar experience into a theme that can be easily understood an talked about. The response should be in a bulleted list format, and I want only the three points without additional explanations. Here is the data to use for your response: ${promptText} `;
 //     console.log("Completion prompt:", completionPrompt);
-
 //     try {
-//       const completion = await openai.chat.completions.create({
-//         model: "gpt-4",
-//         messages: [
-//           {
-//             role: "system",
-//             content: completionPrompt,
-//           },
-//         ],
-//         // max_tokens: 350,
-//         stream: true,
-//       });
-
-//       let responseContent = "";
-//       for await (const chunk of completion) {
-//         responseContent += chunk.choices[0].delta.content;
-//         console.log(chunk.choices[0].delta.content);
-//       }
-
+//       const responseContent = await fetchCompletionFromOpenAI(completionPrompt);
 //       res.json({ content: responseContent });
 //     } catch (error) {
-//       console.error("Error while fetching completion from OpenAI:", error);
-//       return res.status(500).send("Error fetching data from OpenAI");
+//       return res.status(500).send(error.message);
 //     }
 //   }
 // });
+
+router.post("/summarize-career-education", async (req, res) => {
+  const meetingData = req.body[0];
+  const attendees = meetingData.attendees;
+  const attendeeInfos = [];
+
+  for (let attendee of attendees) {
+    const attendeeInfo = await fetchAttendeeData(attendee.attendee_email);
+    attendeeInfos.push(attendeeInfo);
+  }
+
+  for (let i = 0; i < attendeeInfos.length; i++) {
+    const attendeeInfo = attendeeInfos[i];
+    const attendee = attendees[i]; // Assuming the ordering has remained consistent
+
+    const promptText = convertToReadableText(
+      attendeeInfo.experience,
+      attendeeInfo.education,
+      attendee
+    );
+
+    const completionPrompt = `Based on the education and career information provided, please list three rapport-building topics that could be used in conversation. Please be descriptive and use the format as if you are talking to someone as in "I noticed..." or "Your ..." and then the specific observation you wish to share. Each topic should relate specifically to the individual's experiences or background - please focus on unusual things and broadly on why what the person has done in the past has impacted on what they do now. Do not use the same theme more than once. There might be a lot of repetition in experience, in which case, please summarize similar experience into a theme that can be easily understood an talked about. The response should be in a bulleted list format, and I want only the three points without additional explanations. Here is the data to use for your response: ${promptText} `;
+    console.log("Completion prompt:", completionPrompt);
+
+    try {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+          {
+            role: "system",
+            content: completionPrompt,
+          },
+        ],
+        // max_tokens: 350,
+        stream: true,
+      });
+
+      let responseContent = "";
+      for await (const chunk of completion) {
+        responseContent += chunk.choices[0].delta.content;
+        console.log(chunk.choices[0].delta.content);
+      }
+
+      res.json({ content: responseContent });
+    } catch (error) {
+      console.error("Error while fetching completion from OpenAI:", error);
+      return res.status(500).send("Error fetching data from OpenAI");
+    }
+  }
+});
 
 module.exports = router;
