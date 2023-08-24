8a048c239ed0b077a1ec48ecb1748515bc991a78
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-24 10:13:45 +1000

    created insert build rapport command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // External Modules
 const express = require("express");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
 
 // Initialize dotenv
 dotenv.config();
@@ -13,7 +12,6 @@ const app = express();
 const corsMiddleware = require("./middleware/cors");
 app.use(express.json());
 app.use(corsMiddleware);
-app.use(bodyParser.json());
 
 // Internal Modules
 const calendarRoutes = require("./routes/calendarRoutes");
