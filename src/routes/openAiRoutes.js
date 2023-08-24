8e0acc459208c64e03cf353aa69097b4e74ce5d3
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-24 15:09:24 +1000

    created insert build rapport command

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -7,7 +7,6 @@ const { convertToReadableText } = require("../utils/helper");
 router.post("/summarize-career-education", async (req, res) => {
   const meetingData = req.body[0];
   const attendees = meetingData.attendees;
-
   const attendeeInfos = [];
 
   for (let attendee of attendees) {
@@ -16,34 +15,41 @@ router.post("/summarize-career-education", async (req, res) => {
   }
 
   const responses = [];
-  for (let i = 0; i < attendeeInfos.length; i++) {
-    const attendeeInfo = attendeeInfos[i];
-    const attendee = attendees[i]; // Assuming the ordering has remained consistent
-
+  for (let attendeeInfo of attendeeInfos) {
     const promptText = convertToReadableText(
       attendeeInfo.experience,
-      attendeeInfo.education,
-      attendee
+      attendeeInfo.education
     );
 
     const completionPrompt = `Based on the education and career information provided: ${promptText} please list three rapport-building topics that could be used in conversation. Each topic should be a sentence or two and relate specifically to the individual's experiences or background.`;
+    console.log("Completion prompt:", ompletionPrompt);
 
-    const completion = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: [
-        {
-          role: "system",
-          content: completionPrompt,
-        },
-      ],
-      max_tokens: 350,
-      stream: true,
-    });
+    try {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+          {
+            role: "system",
+            content: completionPrompt,
+          },
+        ],
+        max_tokens: 350,
+        stream: true,
+      });
 
-    responses.push(completion.choices[0].message.content);
+      if (completion && completion.choices && completion.choices.length > 0) {
+        responses.push(completion.choices[0].message.content);
+      } else {
+        console.error("Unexpected completion structure:", completion);
+      }
+    } catch (error) {
+      console.error("Error while fetching completion from OpenAI:", error);
+      // If you want to send an error response to the client:
+      // return res.status(500).send("Error fetching data from OpenAI");
+    }
   }
 
-  res.json({ attendeeResponses: responses });
+  res.json({ responses }); // sending back the responses
 });
 
 module.exports = router;
