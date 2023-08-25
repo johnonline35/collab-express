1a226f03fd3cf6149547137b3ce25b34fbc0d40e
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-25 14:13:40 +1000

    adding socket.io

--- src/config/io.js
+++ src/config/io.js
@@ -1,10 +1,27 @@
+const socketIo = require("socket.io");
+
 let io;
 
 module.exports = {
   init: (httpServer) => {
-    io = require("socket.io")(httpServer);
+    const corsOptions = {
+      origin: "*", // IMPORTANT: Replace '*' with your client's URL in production
+      methods: ["GET", "POST"],
+    };
+
+    io = socketIo(httpServer, { cors: corsOptions });
+
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+
+      socket.on("disconnect", () => {
+        console.log("Client disconnected");
+      });
+    });
+
     return io;
   },
+
   getIo: () => {
     if (!io) {
       throw new Error("Socket.io not initialized!");
