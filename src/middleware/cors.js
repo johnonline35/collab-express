7c73a72d24a7dbcb2355e6a7d951c852494a6d5b
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-25 14:41:15 +1000

    adding socket.io

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,17 +2,7 @@ const cors = require("cors");
 
 // Configure CORS middleware
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (
-      origin === "https://www.instantcollab.co" ||
-      origin === "https://www.instantcollab.co;"
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
 };
