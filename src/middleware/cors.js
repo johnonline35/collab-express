353c109cd48be4ceecbadb156cc83e38c27cd168
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-25 14:38:19 +1000

    adding socket.io

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,7 +2,16 @@ const cors = require("cors");
 
 // Configure CORS middleware
 const corsOptions = {
-  origin: "https://www.instantcollab.co",
+  origin: function (origin, callback) {
+    if (
+      origin === "https://www.instantcollab.co" ||
+      origin === "https://www.instantcollab.co;"
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
