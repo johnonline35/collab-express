e45d808961e2b0c86c52b8a0d9bec6649524bbd8
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-30 11:05:02 +1000

    Adding display workspace data column to workspaces table and data enrichment functions

---
+++ src/api/dataEnrichment.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const jobManagerEndpoint = require("../data/collabUrls/jobManagerEndpoint");
+
+async function enrichWorkspacesAndAttendees(
+  workspacesToEnrich,
+  attendeesToEnrich
+) {
+  try {
+    // Process workspacesToEnrich
+    for (let workspace of workspacesToEnrich) {
+      const response = await axios.post(jobManagerEndpoint, workspace);
+      // Handle response if necessary
+      // For example: if(response.status !== 200) { /* Handle error */ }
+    }
+
+    // Process attendeesToEnrich
+    for (let attendee of attendeesToEnrich) {
+      const response = await axios.post(jobManagerEndpoint, attendee);
+      // Handle response if necessary
+    }
+
+    return true; // Successfully enriched both workspaces and attendees
+  } catch (err) {
+    console.error("Error during enrichment:", err);
+    return false; // Indicate a failure in enrichment
+  }
+}
+
+module.exports = {
+  enrichWorkspacesAndAttendees,
+};
