6f8ee9bba3502341080e13e6c75baeba707a8199
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-05 09:01:09 +1100

    Adding display workspace data column to workspaces table and data enrichment functions

--- src/routes/calendarRoutes.js
+++ src/routes/calendarRoutes.js
@@ -25,6 +25,8 @@ router.post("/", async (req, res) => {
     // Fetch the Google calendar data
     const meetingsData = await googleCalendarApiClient.getGoogleCal(userId);
 
+    console.log("$$$$ MEETINGS DATA:", meetingsData);
+
     // Check if a Google Calendar watch is already set up
     const userResult = await checkIfWatchIsSetup(userId);
 
