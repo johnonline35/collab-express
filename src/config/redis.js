0f6dc388c9ee831037e345e0d43b3147e381d6b9
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-20 11:58:44 +1100

    set up redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,6 +3,16 @@ const { REDIS_URL } = require("../config");
 
 const redis = new Redis(REDIS_URL);
 
+redis.set("foo", "bar", (err, result) => {
+  if (err) console.error("Set Error:", err);
+  else {
+    redis.get("foo", (err, value) => {
+      if (err) console.error("Get Error:", err);
+      else console.log(value); // should print "bar"
+    });
+  }
+});
+
 module.exports = {
   redis,
 };
