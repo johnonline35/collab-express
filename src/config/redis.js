502e5e888c81c80611adafc72474a20fbfe7c750
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-20 11:08:31 +1100

    set up redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,21 +1,19 @@
 import Redis from "ioredis";
 const { REDIS_URL } = require("../config");
+console.log("Loading Redis setup...");
 console.log("REDIS_URL:", REDIS_URL);
 
 const redis = new Redis(REDIS_URL);
 
-// Test
-async function testRedis() {
-  try {
-    await redis.set("foo", "bar");
-    let x = await redis.get("foo");
-    console.log(x);
-  } catch (error) {
-    console.error("Error while testing Redis:", error);
+redis.set("foo", "bar", (err, result) => {
+  if (err) console.error("Set Error:", err);
+  else {
+    redis.get("foo", (err, value) => {
+      if (err) console.error("Get Error:", err);
+      else console.log(value); // should print "bar"
+    });
   }
-}
-
-testRedis();
+});
 
 export default {
   redis,
